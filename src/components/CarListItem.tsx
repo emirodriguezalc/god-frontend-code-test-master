bee306697720a87542643f92c713f65e692195e4
Emilia-Rodriguez-Gimenez <e.gimenezrodriguez@doveconviene.com>
2023-05-02 10:45:33 +0200

    Image hover animation

--- src/components/CarListItem.tsx
+++ src/components/CarListItem.tsx
@@ -1,40 +1,67 @@
 import { Block, Flex, Link, Text } from 'vcc-ui';
+import { Transition, TransitionStatus } from 'react-transition-group';
+import { useRef, useState } from 'react';
 
 import { Car } from '../../types/Car';
 
 type CarListItemProps = Car;
 
 const CarListItem = ({ id, modelName, bodyType, modelType, imageUrl }: CarListItemProps) => {
+  const duration = 300;
+
+  const defaultStyle = {
+    transition: `transform ${duration}ms ease-in-out`,
+    transform: 'scale(1)',
+    transformOrigin: 'center',
+    width: '300px',
+    cursor: 'pointer'
+  };
+
+  const transitionStyles: { [key in TransitionStatus]: React.CSSProperties } = {
+    entering: { transform: 'scale(1)' },
+    entered: { transform: 'scale(1.1)' },
+    exiting: { transform: 'scale(1)' },
+    exited: { transform: 'scale(1)' },
+    unmounted: {}
+  };
+
+  const [isHovering, setIsHovering] = useState(false);
+  const nodeRef = useRef<HTMLImageElement>(null);
+
   return (
-    <Flex
-      extend={{
-        padding: '0 12',
-        width: '300px'
-      }}>
+    <Flex extend={{ padding: '0 12', width: '300px' }}>
       <Text extend={{ textTransform: 'uppercase', color: 'var(--v-color-foreground-secondary)' }} variant="bates" subStyle="emphasis">{bodyType}</Text>
       <Text variant="columbus" subStyle="emphasis">{modelName}</Text>
       <Text variant="columbus" subStyle="inline-link">{modelType}</Text>
-      {/* TODO: HOW TO USE NEXT IMAGE WITH DYNAMIC ROUTES? */}
-      <Flex extend={{
-        marginTop: 16
-      }}>
-        {/* INFO: #3 Of the readme */}
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={imageUrl} alt={`A ${modelName} ${modelType} ${bodyType} standing still on grey floor in a studio`} />
+
+      <Flex extend={{ marginTop: 16, overflow: 'hidden' }}>
+        <Transition
+          in={isHovering}
+          timeout={duration}
+          nodeRef={nodeRef}
+          key={modelName}
+        >
+          {state => (
+            <img
+              src={imageUrl}
+              alt={`A ${modelName} ${modelType} ${bodyType} standing still on grey floor in a studio`}
+              style={{ ...defaultStyle, ...transitionStyles[state] }}
+              ref={nodeRef}
+              onMouseEnter={() => setIsHovering(true)}
+              onMouseLeave={() => setIsHovering(false)}
+            />
+          )}
+        </Transition>
       </Flex>
-      <Flex extend={{
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'center',
-        padding: 12
-      }}>
+
+      <Flex extend={{ flexDirection: 'row', width: '100%', justifyContent: 'center', padding: 12 }}>
         <Block extend={{ marginLeft: 12, marginRight: 12 }}>
-          <Link textTransform='none' href={`/learn/${id}`} arrow="right">
+          <Link textTransform="none" href={`/learn/${id}`} arrow="right">
             LEARN
           </Link>
         </Block>
         <Block extend={{ marginLeft: 12, marginRight: 12 }}>
-          <Link textTransform='none' href={`/shop/${id}`} arrow="right">
+          <Link textTransform="none" href={`/shop/${id}`} arrow="right">
             SHOP
           </Link>
         </Block>
